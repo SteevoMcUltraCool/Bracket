a0f3d210fd01b64c54eda04cb05b78cad04a31e9
SteevoMcUltraCool <113132373+SteevoMcUltraCool@users.noreply.github.com>
2022-12-15 13:17:56 -0500

    matched

--- vite-project/matches.js
+++ vite-project/matches.js
@@ -1,4 +1,7 @@
 let mommy = {"DocumentElementObject"}
+let alpha= ["","A","B","C","D","E","F","G","H"]
+let reverseAlpha = {A:1,B:2,C:3,D:4,E:5,F:6,G:7,H:8}
+
 class match {
   constructor(matchID, leftOrRight, round, game, players){
     this.name = `R${round}G${game}`
@@ -31,10 +34,34 @@ class match {
       }
       this.element.appendChild(this[str])
     }
-  
+    mommy.appendChild(this.element)
   }
   setMommy(DOMME){
     mommy = DOMME
   } 
+  setPlayer(num, player){
+    if (typeof(player) == "string") {
+      this[`Player${num}Div`].innerHTML = `${this.game}${num}: ${player}`
+      this.players[num] = new Player(player, `${this.game}${num}`)
+    }else{
+      this[`Player${num}Div`].innerHTML = `${this.game}${num}: ${player.toString()}`
+      this.players[num] = player
+    }
+  }
+  setWinners(first, second){
+    first.sendTo(`R${this.round + 1} G${Alpha[Math.ceil(reverseAlpha[this.game]/2])]}`,1)
+    let secondRound = Math.ceil(reverseAlpha[this.game] + (8/(2**this.round))) % (8/(2**this.round))
+    if (secondRound == 0) {secondRound = (8/(2**this.round))}
+    second.sendTo(`R${this.round + 1} G${Alpha[])]}`,1)
+  }
+}
+class Player {
+  constructor(name,origin) {
+    this.name = name
+    this.origin = origin
+  }
+  toString(){
+    return this.name
+  }
 }
 export let Match = match

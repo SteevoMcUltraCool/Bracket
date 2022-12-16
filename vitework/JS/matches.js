f4ac484c082ed968c10cd5e92d096b4f5db67e47
SteevoMcCool <96311500+SteevoMcCool@users.noreply.github.com>
2022-12-15 21:10:49 -0500

    allows promotion of players

--- vitework/JS/matches.js
+++ vitework/JS/matches.js
@@ -13,7 +13,16 @@ class match { //match is a game played between 4 players where the top 2 advance
     this.element.setAttribute("class", `match ${leftOrRight}`);
     this.header = document.createElement("h5");
     this.header.innerHTML = `Round ${round} Game ${game}`;
+    this.confirmBu = document.createElement("button")
     this.element.appendChild(this.header);
+    let eta = this
+    this.confirmBu.addEventListener("click", function(){
+      let winners = eta.getWinners()
+      eta.setWinners(winners[0],winners[1])
+      eta.confirmBu.remove()
+    })
+    this.confirmBu.innerHTML = "Ok"
+    this.header.appendChild(this.confirmBu)
     Matches[this.name] = this;
     for (let i = 1; i <= 4; i++) {
       let str = `Player${i}Div`;
@@ -26,9 +35,7 @@ class match { //match is a game played between 4 players where the top 2 advance
         this[str].appendChild(this[str].input);
         this.button = document.createElement("button");
         this.button.innerHTML = "Ok";
-        let eta = this
         this.button.addEventListener("click", function (x,y) {
-          console.log(str, eta)
           eta.setPlayer(i, eta[str].input.value);
         },);
         this[str].appendChild(this[str].input);
@@ -38,6 +45,12 @@ class match { //match is a game played between 4 players where the top 2 advance
         }
       } else {
         this[str].innerHTML = `${game}${i}: ${this.players[i] || ""}`;
+        this[str].input =  document.createElement("input")
+        this[str].input.setAttribute("type","number")
+        this[str].input.setAttribute("min","1")
+        this[str].input.setAttribute("max","4")
+        this[str].input.style.maxWidth = "24px"
+        this[str].appendChild(this[str].input)
       }
       this.element.appendChild(this[str]);
     }
@@ -45,30 +58,58 @@ class match { //match is a game played between 4 players where the top 2 advance
   } 
 
   setPlayer(num, player) { //sets the match.player[num]. If player is a player object, we goochie. otherwise, makes a player object with this origin.
+    let str = `Player${num}Div`;
     if (typeof player == "string") {
-      this[`Player${num}Div`].innerHTML = `${this.game}${num}: ${player}`;
+      this[str].innerHTML = `${this.game}${num}: ${player}`;
       this.players[num] = new Player(player, `${this.game}${num}`);
+      //
+      this[str].input =  document.createElement("input")
+      this[str].input.setAttribute("type","number")
+      this[str].input.setAttribute("min","1")
+      this[str].input.setAttribute("max","4")
+      this[str].input.style.maxWidth = "24px"
+      this[str].appendChild(this[str].input)
+      //
     } else {
-      this[`Player${num}Div`].innerHTML = `${
-        this.game
-      }${num}: ${player.toString()}`;
+      this[`Player${num}Div`].innerHTML = `${this.game}${num}: ${player.toString()}`;
       this.players[num] = player;
+      //
+      this[str].input =  document.createElement("input")
+      this[str].input.setAttribute("type","number")
+      this[str].input.setAttribute("min","1")
+      this[str].input.setAttribute("max","4")
+      this[str].input.style.maxWidth = "24px"
+      this[str].appendChild(this[str].input)
+      //
     }
   }
   setWinners(first, second) { //advances the players to their next match. NOT WORKING. intended outcome: winner stays on their path (should work), looser jumps to across board
-    first.sendTo(
-      `R${this.round + 1} G${Alpha[Math.ceil(reverseAlpha[this.game] / 2)]}`,
-      1
-    );
+    first.sendTo(`R${this.round + 1}G${alpha[Math.ceil(reverseAlpha[this.game] / 2)]}`,1);
     let secondRound =
-      Math.ceil(
-        reverseAlpha[this.game] + 8 / 2 ** this.round + (this.round - 1)
-      ) %
-      (8 / 2 ** this.round);
-    if (secondRound == 0) {
-      secondRound = 8 / 2 ** this.round;
+      (Math.ceil(
+        reverseAlpha[this.game] /2 
+      )+ (3 - this.round)) % (8/ 2**this.round);
+    if (secondRound==0){secondRound=(8/ 2**this.round)}
+    console.log(secondRound)
+    second.sendTo(`R${this.round + 1}G${alpha[secondRound]}`, 2);
+  }
+  getWinners(){
+    let first
+    let second
+    let third
+    for (let i = 1; i <= 4; i++) {
+      let str = `Player${i}Div`;
+      if (Number(this[str].input.value)==1) {
+        first = this.players[i]
+      }
+      if (Number(this[str].input.value)==2) {
+        second = this.players[i]
+      }
+      if (Number(this[str].input.value)==3) {
+        third = this.players[i]
+      }          
     }
-    second.sendTo(`R${this.round + 1} G${Alpha[secondRound]}`, 2);
+    return [first, second, third]
   } 
 }
 class Player { // makes a player class
@@ -80,6 +121,7 @@ class Player { // makes a player class
     return this.name;
   }
   sendTo(match, seed) { //sends a player to an open spot in an avaiable match based on their seed. each match past round one should have 2 players with seed==1 and 2 players with seed==2
+    match = Matches[match]
     if (seed == 1) {
       if (match.players[1]) {
         return match.setPlayer(2, this);
@@ -99,3 +141,5 @@ function setMommy(DOMME) { //sets mommy locally
 
 export let Match = match,
   mom = setMommy;
+
+
